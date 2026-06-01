249723c73e6bfeb1a17014761bbf7fd2db5069df
Vaneza <vanezza211@gmail.com>
2026-06-01 15:22:48 -0300

    V1.1

--- src/features_clustering.py
+++ src/features_clustering.py
@@ -20,6 +20,7 @@ def main(
 
     output_path_train: Path = PROCESSED_DATA_DIR / "features_train_cluster.csv",
     output_path_test: Path = PROCESSED_DATA_DIR / "features_test_cluster.csv",
+    output_path_VIF: Path = PROCESSED_DATA_DIR / "features_VIF.csv",
     # -----------------------------------------
 ):
     #---------------------------------------------------------------------
@@ -97,20 +98,76 @@ def main(
                 ('quantil', pipe_quantil, t_quantil)
             ],
         remainder='passthrough' # manter as demais colunas
-        ),
-        StandardScaler() # Aplicar um escalanador universal para k-means
+        )
     )
     # ColumnTRansform gera um array e não um df
 
     #---------------------------------------------------------------------
     # 4. Executar o pipeline
     #---------------------------------------------------------------------
+    from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+    def excluir_vif(df, limite = 5):
+        df_temp = df.select_dtypes(include=[np.number]).copy()
+          
+        excluir = []
+        features_vif = {}
+
+        while True:
+            # Criando df com valores de VIF por features
+            vif_data = pd.DataFrame()
+            vif_data['features'] = df_temp.columns
+            vif_data['VIF'] = [
+                variance_inflation_factor(df_temp.values, i) 
+                for i in range(len(df_temp.columns))
+            ]
+
+            # Encontrar o que tem maior VIF
+            maior_vif = vif_data.sort_values(by='VIF', ascending=False).iloc[0]
+            feature = maior_vif['features']
+            valor_vif = maior_vif['VIF']
+
+            # Se o maior valor VIF > 5, delete
+            if valor_vif > limite:
+                excluir.append(feature) # lista de variaveis a serem excluidas
+                features_vif[feature] = float(valor_vif) # dic com o nome e VIF
+
+                df_temp = df_temp.drop(columns=[feature]) # apagar coluna e recomeçar
+
+            else:
+                break # interromper o loop
+
+        return excluir, features_vif
+
+    # A. Aplicando o pipeline
     logger.info("Executando o pipeline de transformações")
     X_train = pipeline_clustering.fit_transform(df_train)
     X_test = pipeline_clustering.transform(df_test)  
 
-    # Recuperar a ordem e nome exatos da colunas que se perde no ColumnTRansform
+    # B. Recuperar a ordem e nome exatos da colunas que se perde no ColumnTRansform
     colunas_finais = pipeline_clustering.get_feature_names_out().tolist()    
+
+    # C. Aplicar seleção por VIF
+    df_vif = pd.DataFrame(X_train, columns = colunas_finais).astype(float) # df temporario
+    logger.info("Calculando o VIF")
+
+    lista_drop, dicionario_vif = excluir_vif(df_vif, limite=5)
+
+    # D. Filtra as colunas restantes no treino 
+    df_train = df_vif.drop(columns=lista_drop)
+    colunas_restantes = df_train.columns.to_list()
+
+    # E. Filtra as colunas restantes no teste 
+    df_test = pd.DataFrame(X_test, columns = colunas_finais).astype(float) 
+    df_test = df_test[colunas_restantes]
+
+    # F. Aplicar um escalanador universal para k-means
+    scaler = StandardScaler()
+
+    logger.info("Executando a padronização de transformações")
+
+    X_train = scaler.fit_transform(df_train)
+    X_test = scaler.transform(df_test)  
 
     #---------------------------------------------------------------------
     # 5. Salvar Artefatos/Metadados
@@ -125,6 +182,7 @@ def main(
     MODELS_DIR.mkdir(parents=True, exist_ok=True)
 
     # A. Salvar o pipeline completo
+    pipeline_clustering = make_pipeline(pipeline_clustering, scaler)
     joblib.dump(pipeline_clustering, MODELS_DIR / 'pipeline_clustering.pkl')
 
     # Salvar os dados de medianas
@@ -155,6 +213,10 @@ def main(
         'columns_transf_log' : t_log,
         'columns_transf_quantil' : t_quantil,
 
+        # VIF:
+        'removed_by_vif_list': lista_drop,
+        'removed_by_vif_details': dicionario_vif,
+
     }
 
     # C. Salvar json
@@ -166,14 +228,21 @@ def main(
     #---------------------------------------------------------------------
     # 6. Gerar Processed
     #---------------------------------------------------------------------
-    df_train_processed = pd.DataFrame(X_train, columns = colunas_finais)
-    df_test_processed = pd.DataFrame(X_test, columns = colunas_finais)
+    df_train_processed = pd.DataFrame(X_train, columns = colunas_restantes)
+    df_test_processed = pd.DataFrame(X_test, columns = colunas_restantes)
 
     logger.success('Salvando dataset Train em PROCESSED.')
     df_train_processed.to_csv(output_path_train, index = False)
 
     logger.success('Salvando dataset Test em PROCESSED.') 
     df_test_processed.to_csv(output_path_test, index = False)
+
+    logger.success('Salvando infos de VIF PROCESSED.')
+    df_vif_removidas = pd.DataFrame(
+        dicionario_vif.items(),
+        columns=['feature', 'VIF']
+    )
+    df_vif_removidas.to_csv(output_path_VIF, index = False)
 
     logger.success("Etapa de engenharia de features e documentação concluída!")
     logger.success("Uso exclusivo para clusterizacao")
--- src/modeling/train_clustering.py
+++ src/modeling/train_clustering.py
@@ -5,12 +5,11 @@ from loguru import logger
 from tqdm import tqdm
 import typer
 
-import mlflow # biblioteca do MLflow
-import sklearn
 
 from src.config import MODELS_DIR, PROCESSED_DATA_DIR
 
 # Definir e configurar tracking (MLflow)
+import mlflow # biblioteca do MLflow
 mlflow.set_tracking_uri('http://127.0.0.1:5000/')
 mlflow.set_experiment('credit_card_segmentation')
 
@@ -20,15 +19,41 @@ app = typer.Typer()
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "features.csv",
+    features_path: Path = PROCESSED_DATA_DIR / "features_train_cluster.csv",
     labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
     model_path: Path = MODELS_DIR / "model.pkl",
     # -----------------------------------------
 ):
-#%%
+
+    #---------------------------------------------------------------------
+    # 1. Carregar os dados
+    #---------------------------------------------------------------------
+    import pandas as pd
+
+    try:
+        df_processed= pd.read_csv(features_path)
+        logger.info(f'Dataset Treino carregado com sucesso. Formato inicial: {df_processed.shape}')
+    except Exception as e:
+        logger.error(f'Erro a ler arquivo {e}')
+        # forçar o encerramento e dizer ao sistema que tem algo errado
+        raise typer.Exit(code=1)
+
+    #---------------------------------------------------------------------
+    # 2. Definir o numero ideal de clusters
+    #---------------------------------------------------------------------
+    import sklearn
+
+    
+    #---------------------------------------------------------------------
+    # 3. Pipeline do K-means
+    #---------------------------------------------------------------------
+    import sklearn
 
 
-#%%
+
+    #---------------------------------------------------------------------
+    # 4. Treino do modelo
+    #---------------------------------------------------------------------
 
 if __name__ == "__main__":
     app()
